94bdfcb0d8948118853a76b350bc2da62b988be8
Ishan Gupta <93403847+ishan567@users.noreply.github.com>
2026-09-21 14:54:50 +0530

    Delete Sanjay_Kumar_Gupta_Bill_Generator_FINAL_PARTY_NAME_GRADUAL_FONT_REVERIFIED/V5_BUILD directory

--- Sanjay_Kumar_Gupta_Bill_Generator_FINAL_PARTY_NAME_GRADUAL_FONT_REVERIFIED/V5_BUILD/SUPABASE_CUSTOM_PARTICULARS_MIGRATION.sql
+++
@@ -1,4 +0,0 @@
--- Run this once in the existing Supabase project.
--- It is safe to run more than once.
-alter table public.bills
-  add column if not exists custom_items jsonb not null default '[]'::jsonb;
